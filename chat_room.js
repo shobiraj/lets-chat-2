673222c92432d97359f955a8401f75c6772e8a0c
shobiraj <23058337+shobiraj@users.noreply.github.com>
2022-07-21 19:33:29 -0500

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJtJZwYrHT0kkC6wBLgXQ6cyM-DsxaYP8",
+    authDomain: "kwitter-d20a8.firebaseapp.com",
+    databaseURL: "https://kwitter-d20a8-default-rtdb.firebaseio.com",
+    projectId: "kwitter-d20a8",
+    storageBucket: "kwitter-d20a8.appspot.com",
+    messagingSenderId: "221071083955",
+    appId: "1:221071083955:web:53f04aa0917e35a21b23c6"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+ 
